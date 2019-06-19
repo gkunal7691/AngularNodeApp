5f0099caa6c466f2c5ff4d528af2c6aee78e76f0
gkunal7691 <gkunal7691@gmail.com>
2019-06-19 22:18:58 +0530

    node code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testApp';
+
+  constructor(private http : HttpClient)
+  {
+      this.http.get('http://localhost:3000/book').subscribe(
+        (res:any) => {
+          console.log(res);
+          this.title = res.result;
+        }
+      )
+  }
 }
